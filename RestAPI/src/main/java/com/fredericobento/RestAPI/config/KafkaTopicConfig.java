eb87dbb25abcfc9f1456678c26084413d9e22f36
Frederico Bento <fredbento39@gmail.com>
2025-01-31 16:56:11 +0000

    Added kafka config for producer

--- RestAPI/src/main/java/com/fredericobento/RestAPI/config/KafkaTopicConfig.java
+++ RestAPI/src/main/java/com/fredericobento/RestAPI/config/KafkaTopicConfig.java
@@ -1,4 +1,40 @@
 package com.fredericobento.RestAPI.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaTopicConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    public KafkaTopicConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @Bean
+    public NewTopic requestTopic() {
+        return new NewTopic("calc-requests", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic responseTopic() { return new NewTopic("calc-responses", 1, (short) 1); }
+
+    @Bean
+    public ProducerFactory<String ,String> producerFactory() {
+        Map<String, Object> config = new HashMap<>(kafkaProperties.buildProducerProperties());
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
--- RestAPI/src/main/java/com/fredericobento/RestAPI/controller/RestApiController.java
+++ RestAPI/src/main/java/com/fredericobento/RestAPI/controller/RestApiController.java
@@ -2,6 +2,8 @@ package com.fredericobento.RestAPI.controller;
 
 import com.fredericobento.RestAPI.model.CalculationOperation;
 import com.fredericobento.RestAPI.model.CalculationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class RestApiController {
 
+    @Autowired
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public RestApiController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam String name) {
         return "Hello " + name;
@@ -19,6 +28,7 @@ public class RestApiController {
     @GetMapping("/sum")
     public String sum(@RequestParam int a, @RequestParam int b) {
        CalculationRequest request = new CalculationRequest(CalculationOperation.SUM, a, b);
+       kafkaTemplate.send("calc-requests", request.getRequestId(), request.toString());
        return request.toString();
     }
 
