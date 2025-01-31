2e0ca5c1248407b97f582132c9411e352b3649c4
Frederico Bento <fredbento39@gmail.com>
2025-01-31 15:18:16 +0000

    Added RestAPI and Calculator modules with spring boot project skeletons

---
+++ Calculator/src/main/java/com/fredericobento/Calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.fredericobento.Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
