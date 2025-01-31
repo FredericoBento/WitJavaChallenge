2f45b2c1046853ecc136461c470d47186331c5ae
Frederico Bento <fredbento39@gmail.com>
2025-01-31 17:12:31 +0000

    added calculator service kafka listener

--- Calculator/src/main/java/com/fredericobento/Calculator/service/CalculatorService.java
+++ Calculator/src/main/java/com/fredericobento/Calculator/service/CalculatorService.java
@@ -1,4 +1,13 @@
 package com.fredericobento.Calculator.service;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
+
+    @KafkaListener(topics = "calc-requests")
+    public void listen(String message) {
+       System.out.println("Got MsG: " + message);
+    }
 }
